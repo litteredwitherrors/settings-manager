36a2796620916d1e1615e80e05fae895da40f7e6
Dexter Lesaca <emobile@gmail.com>
2015-08-06 12:57:54 -0700

    cleanup styles

--- app/js/services/resourceService.js
+++ app/js/services/resourceService.js
@@ -1,31 +1,31 @@
 'use strict';
 
 module.exports = function(app) {
-	var errorHandler = function(data) {
-		console.log(data);
-	};
+  var errorHandler = function(data) {
+    console.log(data);
+  };
 
-	app.factory('resource', ['$http', function($http) {
-		return function(resourceName) {
-			return {
-				getAll: function(callback) {
-					$http({
-						method: 'GET',
-						url: '/' + resourceName
-					})
-					.success(callback)
-					.error(errorHandler);
-				},
-				submit: function(resource, callback) {
-					$http({
-						method: 'POST',
-						url: '/' + resourceName,
-						data: resource
-					})
-					.success(callback)
-					.error(errorHandler);
-				},
-				update: function(id, data, callback) {
+  app.factory('resource', ['$http', function($http) {
+    return function(resourceName) {
+      return {
+        getAll: function(callback) {
+          $http({
+            method: 'GET',
+            url: '/' + resourceName
+          })
+          .success(callback)
+          .error(errorHandler);
+        },
+        submitForm: function(resource, callback) {
+          $http({
+            method: 'POST',
+            url: '/' + resourceName,
+            data: resource
+          })
+          .success(callback)
+          .error(errorHandler);
+        },
+        update: function(id, data, callback) {
           $http({
             method: 'PUT',
             url: '/' + resourceName + '/' + id,
@@ -34,16 +34,16 @@ module.exports = function(app) {
           .success(callback)
           .error(errorHandler);
         },
-				destroy: function(id, callback) {
-					$http({
-						method: 'DELETE',
-						url: '/' + resourceName + '/' + id,
-						data: id
-					})
-					.success(callback)
-					.error(errorHandler);
-				}
-			}
-		}
-	}]);
+        destroy: function(id, callback) {
+          $http({
+            method: 'DELETE',
+            url: '/' + resourceName + '/' + id,
+            data: id
+          })
+          .success(callback)
+          .error(errorHandler);
+        }
+      }
+    }
+  }]);
 };
--- app/js/settings/controllers/settingsController.js
+++ app/js/settings/controllers/settingsController.js
@@ -1,42 +1,42 @@
 'use strict';
 
 module.exports = function(app) {
-	app.controller('appController', ['$scope', 'resource', function($scope, resource) {
+  app.controller('appController', ['$scope', 'resource', function($scope, resource) {
 
-		var Setting = resource('settings');
+    var Setting = resource('settings');
 
-		$scope.getSettings = function(){
-			Setting.getAll(function(response){
-				console.log(response);
-				$scope.settings = response;
-			});
-		};
+    $scope.getSettings = function(){
+      Setting.getAll(function(response){
+        console.log(response);
+        $scope.settings = response;
+      });
+    };
 
-		$scope.submitForm = function(setting) {
-			console.log('submitted');
-			Setting.submitForm(setting, function(response) {
-				$scope.getSettings();
-			});
-		};
+    $scope.submitForm = function(setting) {
+      console.log('submitted');
+      Setting.submitForm(setting, function(response) {
+        $scope.getSettings();
+      });
+    };
 
-		$scope.destroy = function(id) {
-			console.log(id);
-			Setting.destroy(id, function(response) {
-				$scope.getSettings();
-			});
-		}
+    $scope.destroy = function(id) {
+      console.log(id);
+      Setting.destroy(id, function(response) {
+        $scope.getSettings();
+      });
+    }
 
-		$scope.update = function(id, setting) {
-			console.log(id);
-			Setting.update(id, setting, function(response) {
-				setting.editing = false;
-				$scope.getSettings();
-			});
-		};
+    $scope.update = function(id, setting) {
+      console.log(id);
+      Setting.update(id, setting, function(response) {
+        setting.editing = false;
+        $scope.getSettings();
+      });
+    };
 
-		$scope.edit = function(setting) {
-			setting.editing = true;
-			console.log(setting);
-		};
-	}]);
+    $scope.edit = function(setting) {
+      setting.editing = true;
+      console.log(setting);
+    };
+  }]);
 };
